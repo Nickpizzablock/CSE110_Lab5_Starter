563d0a8c0f456fa63989268b9e4eff2fb798d891
Nickpizzablock <ndtorres@ucsd.edu>
2024-05-07 22:05:13 -0700

    finished writing the tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 5 - Starter
+1. I would not test the message feature because that falls under the cons portion where we can check if a send function is compatible with some recieve function. Since there are 2 parts trying to be tested, it is not suitable for testing.
+2. I would test the max message length feature because that is something that can be easily tested. It is encapsulated because this only deals with the textbox and not any other function of the program.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,73 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
+// isPhoneNumber
+test('isPhoneNumber - 6192121234', () => {
+  expect(isPhoneNumber('6192121234')).toBe(false);
+});
+test('isPhoneNumber - (619)2121234', () => {
+  expect(isPhoneNumber('(619)2121234')).toBe(false);
+});
+test('isPhoneNumber - (619)212-1234', () => {
+  expect(isPhoneNumber('(619)212-1234')).toBe(true);
+});
+test('isPhoneNumber - (619) 212-1234', () => {
+  expect(isPhoneNumber('(619) 212-1234')).toBe(true);
+});
+
+// isEmail
+test('isEmail - ndtorres@ucsd.edu', () => {
+  expect(isEmail('ndtorres@ucsd.edu')).toBe(true);
+});
+test('isEmail - a@a.com', () => {
+  expect(isEmail('a@a.com')).toBe(true);
+});
+test('isEmail - ndtorres@ucsd.megagamer', () => {
+  expect(isEmail('ndtorres@ucsd.megagamer')).toBe(false);
+});
+test('isEmail - ndtorres@ucsd.', () => {
+  expect(isEmail('ndtorres@ucsd.')).toBe(false);
+});
+
+// isStrongPassword
+test('isStrongPassword - 1234', () => {
+  expect(isStrongPassword('1234')).toBe(false);
+});
+test('isStrongPassword - 123abcefg', () => {
+  expect(isStrongPassword('123abcefg')).toBe(false);
+});
+test('isStrongPassword - alkudfa_123', () => {
+  expect(isStrongPassword('alkudfa_123')).toBe(true);
+});
+test('isStrongPassword - WhereIsTheLamb', () => {
+  expect(isStrongPassword('WhereIsTheLamb')).toBe(true);
+});
+
+// isDate
+test('isDate - 12/15/1999', () => {
+  expect(isDate('12/15/1999')).toBe(true);
+});
+test('isDate - 1/1/1969', () => {
+  expect(isDate('1/1/1969')).toBe(true);
+});
+test('isDate - 69/420/69420', () => {
+  expect(isDate('69/420/69420')).toBe(false);
+});
+test('isDate - 1-1-1', () => {
+  expect(isDate('1-1-1')).toBe(false);
+});
+
+// isHexColor
+test('isHexColor - #1', () => {
+  expect(isHexColor('#1')).toBe(false);
+});
+test('isHexColor - #11111', () => {
+  expect(isHexColor('#11111')).toBe(false);
+});
+test('isHexColor - #111', () => {
+  expect(isHexColor('#111')).toBe(true);
+});
+test('isHexColor - #537257', () => {
+  expect(isHexColor('#537257')).toBe(true);
+});
 // TODO - Part 2
